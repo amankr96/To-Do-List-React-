d2b6c0c51fad87a7d829ab3bf1b81bafd7a683f1
amankr96 <aman.kr96@gmail.com>
2021-01-28 14:58:07 +0530

    Adding comments

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,10 +5,10 @@ const ToDoList=(props)=>{
         <React.Fragment>
         <div className="todo_style">
         <i className="fa fa-times" aria-hidden="true" 
-        onClick={()=>{
+        onClick={()=>{                                  /* For the delete operation we want to know which of the item is being clicked. We want that id. Now let' go to the delete method in App.jsx*/
             props.onSelect(props.id);
         }}/>
-        <li> {props.text} </li>
+        <li> {props.text} </li>     
         </div>
         </React.Fragment>
         );
